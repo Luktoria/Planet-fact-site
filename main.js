94d404e65429f070afde19a2307313930854ecf8
Luktoria <110913006+Luktoria@users.noreply.github.com>
2023-10-03 17:46:56 +0200

    Earth-page-tablet-mobile-ready

--- main.js
+++ main.js
@@ -1,43 +1,36 @@
 $(document).ready(() => {    
-
-$('#structure-button').on('click', () => {
+ 
+    
+$('.structure-button').on('click', () => {
      $('#earth-img').attr("src", "./assets/planet-earth-internal.svg");
-     $('#overview-button').css('background-color', '#070724');
-     $('#structure-button').css('background-color', '#6f2ed6');
-     $('#surface-button').css('background-color', '#070724');
+     $('.overview-button').css('background-color', '#070724'); 
+     $('.structure-button').css('background-color', '#6f2ed6');
+     $('.surface-button').css('background-color', '#070724'); 
      $('#earth-overview-paragraph').css('display', 'none');
      $('#earth-interior-paragraph').css('display', 'block');
      $('#earth-geology-paragraph').css('display', 'none');
      $('#earth-geology-img').css('display', 'none');
      $('#anchor-ref').attr("href", "https://en.wikipedia.org/wiki/Earth#Internal_structure");
-})/*.on('mouseenter', () => {
-    $("#structure-button").css('background-color', '#38384F');
-}).on('mouseleave', () => {
-    $("#structure-button").css('background-color', 'transparent');
-})*/;
+});
 
 
-$('#overview-button').on('click', () => {
+$('.overview-button').on('click', () => {
     $('#earth-img').attr("src", "./assets/planet-earth.svg");
-    $('#structure-button').css('background-color', '#070724');
-    $('#overview-button').css('background-color', '#6f2ed6');
-    $('#surface-button').css('background-color', '#070724');
+    $('.structure-button').css('background-color', '#070724'); 
+    $('.overview-button').css('background-color', '#6f2ed6');
+    $('.surface-button').css('background-color', '#070724'); 
     $('#earth-overview-paragraph').css('display', 'block');
     $('#earth-interior-paragraph').css('display', 'none');
     $('#earth-geology-paragraph').css('display', 'none');
     $('#earth-geology-img').css('display', 'none');
     $('#anchor-ref').attr("href", "https://en.wikipedia.org/wiki/Earth");
-})/*.on('mouseenter', () => {
-    $("#overview-button").css('background-color', '#38384F');
-}).on('mouseleave', () => {
-    $("#overview-button").css('background-color', 'inherit');
-})*/;
+});
 
-$('#surface-button').on('click', () => {
+$('.surface-button').on('click', () => {
     $('#earth-geology-img').css('display', 'block');
-    $('#surface-button').css('background-color', '#6f2ed6');
-    $('#structure-button').css('background-color', '#070724');
-    $('#overview-button').css('background-color', '#070724');
+    $('.surface-button').css('background-color', '#6f2ed6');
+    $('.structure-button').css('background-color', '#070724');
+    $('.overview-button').css('background-color', '#070724'); 
     $('#earth-geology-paragraph').css('display', 'block');
     $('#earth-overview-paragraph').css('display', 'none');
     $('#earth-interior-paragraph').css('display', 'none');
@@ -45,9 +38,12 @@ $('#surface-button').on('click', () => {
 })
 
 
+$('#hamburger-icon').on('click', () => {
+    $('.planet-variations-mobile').toggle(500);
+    $('#earth-geology-img').css('display', 'none');
 
-
-
+}) 
+ 
 
 
 
